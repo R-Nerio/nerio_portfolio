b3c5545427c51aa6d70b29eba99d2bb53ef38ffb
R-Nerio <r.nerio.477839@umindanao.edu.ph>
2024-08-28 15:10:27 +0800

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,15 @@ function App() {
       return (
         <h1 className="text-3xl font-bold underline">
           Hello world!
+          Hello world!
+          Hello world!
+          Hello world!
+          Hello world!
+          Hello world!
+          Hello world!
+          Hello world!
+          Hello world!
+          Hello world!
         </h1>
       )
     }
